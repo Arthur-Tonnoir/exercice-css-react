272c0531a7ec5fa088d5492702b278941b67826d
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2023-06-13 11:34:55 +0200

    feat: add component photo :sparkles:

---
+++ src/Components/Photo/Photo.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './Photo.css';
+import '../../style.css';
+
+function Photo(){
+    return (
+        <div className='main-photo'>
+            <div className='containers'>
+                <div className='container-bleu'>
+                    <div className='container-blanc'>
+                        <img className='yellow' src='' alt='photo'/>
+                    </div>
+                </div>
+                <div className='identite-container'>
+                    <div className='identite'><h4>Arthur TONNOIR</h4></div>
+                    <div className='job'>Développeur Back-End</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Photo;
