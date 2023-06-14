203c5e90801e1b3e9247c6f2de46791486e229fe
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2023-06-14 15:26:20 +0200

    feat: add icon to GridLeft :sparkles:

--- src/Components/GridLeft/GridLeft.js
+++ src/Components/GridLeft/GridLeft.js
@@ -10,28 +10,32 @@ function GridLeft(){
           <div className='block-title'><h4>CONTACT</h4></div>
           <div className='adresse'>
             <div className='image'>
-              <img src='' alt='adresse'/>
+            <img width="48" height="48" src="https://img.icons8.com/ios-filled/48/address--v1.png" alt="addresse"/>
             </div>
             <div className='text'><p>1 rue Gambetta <br/> 62740 Fouquierès-Lès-Lens</p></div>
           </div>
           <div className='email'>
             <div className='image'>
-              <img src='' alt='email'/>
+            <img width="48" height="48" src="https://img.icons8.com/color/48/email.png" alt="email"/>
             </div>
             <div className='text'><p>arthur.tonnoir.pro@sfr.fr</p></div>
           </div>
           <div className='numero'>
           <div className='image'>
-              <img src='' alt='phone'/>
+          <img width="48" height="48" src="https://img.icons8.com/ios-filled/48/phone.png" alt="phone"/>
             </div>
             <div className='text'><p>06.42.20.40.98</p></div>
           </div>
           <div className='linkedin'>
-            <div className='title'><p>Linkedin</p></div>
+            <div className='image'>
+              <img width="48" height="48" src="https://img.icons8.com/color/48/linkedin.png" alt="linkedin"/> 
+            </div>
             <div className='text'><a href='https://linkedin.com/in/arthur-tonnoir'>https://linkedin.com/in/arthur-tonnoir</a> </div>
           </div>
           <div className='github'>
-          <div className='title'><p>GitHub</p></div>
+            <div className='image'>
+            <img width="48" height="48" src="https://img.icons8.com/ios-filled/48/github.png" alt="github" title='github'/> 
+            </div>
             <div className='text'><a href='https://github.com/Arthur-Tonnoir'>https://github.com/Arthur-Tonnoir</a></div>
           </div>
         </div>
