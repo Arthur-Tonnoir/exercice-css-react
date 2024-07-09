e641310b4bcbc3ac4fde0d4874fdc8f51819e246
Arthur Tonnoir <arthur1.tonnoir@sfr.fr>
2024-07-09 10:49:11 +0200

    feat: add photo img :sparkles:

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './Photo.css';
 import '../../style.css';
+import photo from '../../resource//assets/jpg/photo.jpg'
 
 function Photo(){
     return (
@@ -9,7 +10,7 @@ function Photo(){
                 <div className='container-bleu'>
                     <div className='identite-container'>
                         <div className='container-blanc'>
-                        <img src='' alt="photo"/>
+                        <img src={photo} alt="arthur"/>
                         </div>
                         <div className='block-identite'>
                         <div className='identite'><h4>Arthur TONNOIR</h4></div>
